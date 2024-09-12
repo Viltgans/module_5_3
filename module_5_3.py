d81c3d265bbc57ffb3194b6e734a681295f682aa
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-09-12 21:28:40 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -7,7 +7,8 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
     def __add__(self, value):
         if isinstance(value, int):
@@ -16,28 +17,31 @@ class House:
 
     def __iadd__(self, value):
         if isinstance(value, int):
-            self.number_of_floors += value
-        return self
+            return self.__add__(value)
 
     def __radd__(self, value):
         if isinstance(value, int):
-            self.number_of_floors = value + self.number_of_floors
-        return self
+            return self.__add__(value)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -45,19 +49,14 @@ h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
-
 print(h1 == h2)
-
 h1 = h1 + 10
 print(h1)
 print(h1 == h2)
-
 h1 += 10
 print(h1)
-
 h2 = 10 + h2
 print(h2)
-
 print(h1 > h2)
 print(h1 >= h2)
 print(h1 < h2)
